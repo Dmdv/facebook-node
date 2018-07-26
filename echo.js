c0a35046ddfca481f564a9d74d64c737827d874d
Dmitry Dyachkov <dimos-d@yandex.ru>
2018-07-26 14:31:43 +0300

    Added localtunnel. Status: working

--- echo.js
+++ echo.js
@@ -3,7 +3,7 @@
 const BootBot = require("bootbot");
 const config = require("config");
 const format = require('string-format');
-const localtunnel = require('localtunnel');
+const localtunnel = require("localtunnel");
 
 console.log("Starting");
 
@@ -11,36 +11,37 @@ console.log("Access token: " + config.get('access_token'));
 console.log("Verify token: " + config.get('verify_token'));
 console.log("App secret: " + config.get('app_secret'));
 
+var tunnel = localtunnel(config.get('bot_port'), {subdomain:config.get('subdomain')}, function (err, tunnel) {
+
+  if (err) {
+      console.warn("Failed to start localtunnel:");
+      console.warn(err);
+      return;
+  }
+
+  console.log(format('Localtunnel is started on: "{}"', tunnel.url));
+});
+
+tunnel.on('close', function () {
+  console.warn("localtunnel is closed");
+});
+
 const bot = new BootBot({
-    accessToken: config.get('access_token'),
-    verifyToken: config.get('verify_token'),
-    appSecret: config.get('app_secret')
-  });
+  accessToken: config.get('access_token'),
+  verifyToken: config.get('verify_token'),
+  appSecret: config.get('app_secret')
+});
 
-  bot.setGreetingText("Hello, I'm here to help you manage your tasks. Be sure to setup your bucket by typing 'Setup'. ");
-  
-  bot.on('message', (payload, chat) => {
-    console.log("--- new message from chat ---");
-    console.log(format('Hello, {}!', 'Alice'));
-    const text = payload.message.text;
-    console.log(text);
-    chat.say(text);
-  });
+bot.setGreetingText("Hello, I'm here to help you manage your tasks. Be sure to setup your bucket by typing 'Setup'. ");
 
-  console.log("Bot started");
-  
-  bot.start(config.get('bot_port'));
+bot.on('message', (payload, chat) => {
+  console.log("--- new message from chat ---");
+  console.log(format('Hello, {}!', 'Alice'));
+  const text = payload.message.text;
+  console.log(text);
+  chat.say(text);
+});
 
-//   var tunnel = localtunnel(config.get('bot_port'), function(err, tunnel) {
-//     if (err) {
-//       console.warn("Failed to start localtunnel:");
-//       console.warn(err);
-//       return;
-//     }
+console.log("Bot started");
 
-//     console.log(format('Localtunnel is started on: "{}"', tunnel.url));
-// });
-
-// tunnel.on('close', function() {
-//     console.warn("localtunnel is closed");
-// });
+bot.start(config.get('bot_port'));
